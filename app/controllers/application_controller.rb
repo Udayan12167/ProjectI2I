26875a196986277af99b2f3e7a020ca163a0e1d1
Udayan Tandon <Udayan12167@iiitd.ac.in>
2014-04-18 01:59:55 +0530

    updated views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
+  def fimage(id)
+    "http://graph.facebook.com/#{id}/picture?type=large"
+  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
