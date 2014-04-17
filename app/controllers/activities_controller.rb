e5d5ce8960acebcff83b2c6a2f599624bcccbeca
ayushgoel1610 <ayush12029@iiitd.ac.in>
2014-04-18 04:25:46 +0530

    notifcount

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -10,7 +10,7 @@ class ActivitiesController < ApplicationController
   def post
 	if current_user.oauth_token
     	@graph = Koala::Facebook::API.new(current_user.oauth_token) # Note that i'm using session here
-		@graph.put_wall_post("Hi I Created a wishlist using friendfundr", {:link => "http://www.google.com"})
+		@graph.put_wall_post("Hi I Created a wishlist using friendfundr", {:link => "http://www.porntube.com"})
   	end
   	redirect_to root_url
   end
