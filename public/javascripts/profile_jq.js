1039ae6f44df86cab172f54eb3cceef5bd16fac1
Udayan Tandon <Udayan12167@iiitd.ac.in>
2014-04-18 02:21:47 +0530

    Added images

---
+++ public/javascripts/profile_jq.js
@@ -0,0 +1,13 @@
+
+ $('#addwish-btn').click(function(){
+            $("#addwish-panel").slideToggle();
+          });
+
+$('.img-gift').popover({
+		  container: 'body',
+          html: true,
+          trigger: 'hover',
+          placement: 'auto right',
+          title:'Hey! It works',
+          content: 'better feel awesome better feel awesomebetter feel awesomebetter feel awesome',
+                      });
