cc2ec73ff55eb4ef1cae893266b25c2c3fa53d01
vedantswain <vedant12118@iiitd.ac.in>
2014-04-22 15:04:50 -0700

    friends allignment, panel toggle

--- public/javascripts/profile_jq.js
+++ public/javascripts/profile_jq.js
@@ -5,11 +5,14 @@
 
 
  $('#invite-btn').click(function(){
-            $('#invite-panel').slideToggle('medium', function() {
-    if ($('#invite-panel-panel').is(':visible'))
-        $('#invite-panel').css('display','inline-block');
-});
-        });
+            $("#invite-panel").slideToggle();
+            $("#friends-panel").toggle();
+            if ($(this).text() == "Invite Friends")
+		       $(this).text("View Friends")
+		    else
+		       $(this).text("Invite Friends");
+          });
+
 
 $('#notification').click(function(){
 	$('#notification').css({
@@ -17,21 +20,11 @@ $('#notification').click(function(){
 	});
 })
 
-$('.img-gift:even').popover({
-          container: 'body',
-          html: true,
-          trigger: 'hover',
-          placement: 'auto left',
-          title:'Hey! It works',
-          content: 'better feel awesome TEDGlobal is heading to Brazil! In October 2014, in Rio de Janeiro, we will celebrate the outpouring of innovation, energy and creativity taking place all over South America and the global south. The world is changing fast. Fresh thinking can be found in every direction, in emerging geographies as well as in the western hemisphere. Join us in Rio to explore the realities of tomorrow.',
-                      });
-
-
-$('.img-gift:odd').popover({
-          container: 'body',
+$('.img-gift').popover({
+		  container: 'body',
           html: true,
           trigger: 'hover',
           placement: 'auto right',
           title:'Hey! It works',
-          content: 'better feel awesome TEDGlobal is heading to Brazil! In October 2014, in Rio de Janeiro, we will celebrate the outpouring of innovation, energy and creativity taking place all over South America and the global south. The world is changing fast. Fresh thinking can be found in every direction, in emerging geographies as well as in the western hemisphere. Join us in Rio to explore the realities of tomorrow.',
+          content: 'better feel awesome better feel awesome better feel awesome better feel awesome',
                       });
