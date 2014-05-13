7d517b9a5e43cebfa728eb6b008bf880a02fc965
ayushgoel1610 <ayush12029@iiitd.ac.in>
2014-05-13 17:53:22 +0530

    likes

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -17,9 +17,35 @@ class WishlistsController < ApplicationController
   def initiate
     @wishlist =current_user.wishlist.build
   end
+
+  def vote
+      @object = params[:my_param]
+      current_user.wishlist.each do |t|
+      if @object.to_i == t.id.to_i
+        current_user.like!(t)
+      end
+
+    end
+   redirect_to root_url  
+    end
+  def unvote
+      @object = params[:my_param]
+      current_user.wishlist.each do |t|
+      if @object.to_i == t.id.to_i
+        current_user.unlike!(t)
+      end
+
+    end
+    redirect_to root_url  
+    end
+
   private
 
     def wishlist_params
       params.require(:wishlist).permit(:link,:price)
     end
+
+    
+
+  
 end
