eaddcf597a1abad0b67af4d677684c0bf60ad3fe
ayushgoel1610 <ayush12029@iiitd.ac.in>
2014-05-13 15:38:03 +0530

    llike

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  acts_as_liker
   has_many :friendships
   has_many :friends,
    :through => :friendships,
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -2,6 +2,7 @@ class Wishlist < ActiveRecord::Base
 	include PublicActivity::Model
   	tracked owner: ->(controller, model) { controller && controller.current_user }
 	belongs_to :user
+	acts_as_likeable
 	validates :user_id, presence: true
 	validates :link, presence: true,length: {maximum: 140}
 	
